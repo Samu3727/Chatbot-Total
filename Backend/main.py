41fc740351ad87b9d170bf6c35feed51643bd56d
Samuel Esquivel <samuelcuidaesquivel@gmail.com>
2026-02-18 21:32:51 -0500

    Se actualizo ruteo del uvicorn para seguridad

--- Backend/main.py
+++ Backend/main.py
@@ -34,4 +34,4 @@ app.include_router(chat_router, prefix="/api/v1")
 if __name__ == "__main__":
     import uvicorn
     # Usar 0.0.0.0 para permitir conexiones desde la red local y emuladores
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
